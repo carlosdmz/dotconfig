5c94fc75e02224c1e6a254b1df01d486f13ce468
Carlos Damazio <hello@carlosdmz.io>
2026-09-22 12:39:28 -0300

    feat: adding new nvim startup file
    Also adding the mason plugin

---
+++ nvim/pack/init.lua
@@ -0,0 +1,22 @@
+-- Set line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Tabs and indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- UI settings
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex,   { desc = "Open file explorer" })
+vim.keymap.set("n", "<leader>w", vim.cmd.write, { desc = "Write file" })
+vim.keymap.set("n", "<leader>q", vim.cmd.quit,  { desc = "Quit a single window"})
+vim.keymap.set("n", "<leader>qa", vim.cmd.qall, { desc = "Quit vim altogether"})
+vim.keymap.set("i", "jk", vim.cmd.stopinsert,   { desc = "Switch from INSERT to NORMAL mode" })
+
